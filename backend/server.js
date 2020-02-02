6e820a5a6737b9f5ca9e0a0f587171d66b4d57cd
Ekansh Jain <ejekanshjain@gmail.com>
2020-02-02 15:54:34 +0530

    adjusted my styling and initialised backend server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// Imports
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+// Initializing app
+const app = express()
+
+// Setting up middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+
+// Server status
+app.get('/', (req, res) => res.status(200).json({ status: 200, message: 'Server up and running' }))
+app.get('/api', (req, res) => res.status(200).json({ status: 200, message: 'API server up and running' }))
+
+// Starting Server
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server Started on Port ${port}`))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  )
+    return (
+        <div>
+            Hello World
+        </div>
+    )
 }
 
 export default App
