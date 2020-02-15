c8a047465793f6b02b9aec356765e6ba1c5eb140
Ekansh Jain <ejekanshjain@gmail.com>
2020-02-15 11:08:32 +0530

    Update authApiKey.js

--- backend/middlewares/authApiKey.js
+++ backend/middlewares/authApiKey.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = (req, res, next) => {
     const apiKey = req.headers['x-api-key']
-    if (apiKey == null || apiKey != process.env.X_API_KEY) return res.status(401).json({ status: 401, message: 'Unauthorised client' })
+    if (apiKey == null || apiKey != process.env.X_API_KEY) return res.status(403).json({ status: 403, message: 'Unauthorised client' })
     next()
 }
